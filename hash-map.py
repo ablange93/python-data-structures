54f0d2810e6b7a8ddd97b676c2f74cfedbe05037
Andrew Lange <andrew.lange93@gmail.com>
2018-10-08 16:32:04 -0400

    Implemented Setter function to assign key-value pairs to underlying Array.

--- hash-map.py
+++ hash-map.py
@@ -5,7 +5,7 @@ class HashMap:
     self.array_size = array_size
     self.array = [None for item in range(array_size)]
     
-  # Hash | User inputs a key which is converted into bytes
+  # Hash | User inputs a key which is converted into bytes. #
   def hash(self, key):
     key_bytes = key.encode()
     hash_code = sum(key_bytes)
@@ -14,3 +14,7 @@ class HashMap:
   # Compressor | Ensure hash code will fit inside the array. #
   def compressor(self, hash_code):
     return hash_code % self.array_size
+    
+  # Setter | Assigns key-value pair to array. #
+  def assign(self, key, value):
+    self.array[self.compressor(self.hash(key))] = value
