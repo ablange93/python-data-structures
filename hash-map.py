3064934678aeb95e3c879b58ee0964900dede9ab
Andrew Lange <andrew.lange93@gmail.com>
2018-10-08 15:47:31 -0400

    Added Hash Map class.

---
+++ hash-map.py
@@ -0,0 +1,8 @@
+# HASH MAP #
+class HashMap:
+  # Array | Create the array
+  def __init__(self, array_size):
+    self.array_size = array_size
+    self.array = [None for item in range(array_size)]
+    
+
