adf872b2d1f59b8ba16f18c1756f08e46f2fc2ca
Andrew Lange <andrew.lange93@gmail.com>
2018-10-09 20:00:04 -0400

    Succesfully assigned and retrieved 3 key-value pairs.

--- hash-map.py
+++ hash-map.py
@@ -86,4 +86,16 @@ class HashMap:
 	  # SUB-COLLISION EVENT | Increment counter and re-iterate
       number_collisions += 1
     return
+    
+# MAIN #
+hash_map = HashMap(15)
+
+hash_map.assign('gabbro', 'igneous')
+hash_map.assign('sandstone', 'sedimentary')
+hash_map.assign('gneiss', 'metamorphic')
+
+
+print(hash_map.retrieve('gabbro'))
+print(hash_map.retrieve('sandstone'))
+print(hash_map.retrieve('gneiss'))
       
